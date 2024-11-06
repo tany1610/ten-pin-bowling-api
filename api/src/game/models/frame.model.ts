2387af97485ec5c45129f06d2ad4acf0b3fb7df0
Tanya Staneva <33036767+tany1610@users.noreply.github.com>
2024-11-06 11:20:23 +0200

    refactor and reduce code complexity

--- api/src/game/models/frame.model.ts
+++ api/src/game/models/frame.model.ts
@@ -1,19 +1,12 @@
 export class Frame {
     private pins: number = 10;
+    private readonly totalPins: number = 10;
     private readonly _firstRoll: number;
     private readonly _secondRoll: number;
-    private _isStrike: boolean;
-    private _isSpare: boolean;
 
-    constructor() {
-        this._firstRoll = this.getRandomPins();
-        this._secondRoll = this.getRandomPins();
-
-        if (this._firstRoll === 10) {
-            this._isStrike = true;
-        } else if (this._firstRoll + this._secondRoll === 10) {
-            this._isSpare = true;
-        }
+    constructor(firstRoll?: number, secondRoll?: number) {
+        this._firstRoll = firstRoll !== undefined ? firstRoll : this.getRandomPins();
+        this._secondRoll = secondRoll !== undefined ? secondRoll : this.getRandomPins();
     };
 
     private getRandomPins(): number {
@@ -22,8 +15,8 @@ export class Frame {
         return hitPins;
     }
 
-    public get isStrike(): boolean { return  this._isStrike; };
-    public get isSpare(): boolean { return this._isSpare; };
+    public get isStrike(): boolean { return  this._firstRoll === this.totalPins; };
+    public get isSpare(): boolean { return !this.isStrike && this._firstRoll + this._secondRoll === this.totalPins; };
     public get firstRoll(): number { return this._firstRoll; };
     public get secondRoll(): number { return this._secondRoll; };
 }
